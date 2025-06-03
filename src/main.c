77e12127d6b473cb183e5f844c353e3e4fbe4d39
Pareena Sumbli <pareenasumbli@gmail.com>
2025-06-02 20:35:19 -0400

    remove the newline character for the error output

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  input[strlen(input) - 1] = '\0'; // Remove newline character
   printf("%s: command not found\n", input);
   return 0;
 }
